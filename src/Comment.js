05bab2326eddd211a93b36d7f6cad2444bd04fe3
Roman <kaboom.ua@gmail.com>
2017-04-26 17:13:26 +0300

    Update Comment.js

--- src/Comment.js
+++ src/Comment.js
@@ -1,6 +1,7 @@
 'use strict';
 import React, {Component} from 'react'
 
+//Это можно сделать Functional Component, по возможности используй их
 export default class Comment extends Component {
     render() {
         return (
